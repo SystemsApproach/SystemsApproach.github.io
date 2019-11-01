8ef4ab375c811af4486eb6d9943bcd00edcc7a5e
Larry Peterson <llp@opennetworking.org>
2019-11-01 13:56:09 -0700

    publish latest

--- static/documentation_options.js
+++ static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Version 6.1',
+    VERSION: 'Version 6.1-dev',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
